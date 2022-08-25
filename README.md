7b603dcdc74254de44a45bd02c8f53230040f584
TaufiqHassan <taufiq.hassan@pnnl.gov>
2022-08-25 16:27:59 -0700

    README.rst added

--- README.md
+++
@@ -1,2 +0,0 @@
-# asediag
-Aerosol process analysis in model-native Spectral Element (SE) grid
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as readme_file:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
 requirements = [
